a981e49880dc33975a2fb5118bc3d7880a5a2a35
Patel Muhammed <muhammedpatel538@gmail.com>
2024-07-15 07:05:05 +0530

    solution

--- 2196.cpp
+++ 2196.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 /*
-    2196. Create Binary Tree From Descriptions
+ * 2196. Create Binary Tree From Descriptions
 */
 
 struct TreeNode {
@@ -14,24 +14,27 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
-    TreeNode* valueToNode[100001] = {};
-    bool isChildNode[100001] = {};
-    for (auto& d : descriptions){
-        if (valueToNode[d[0]] == nullptr) valueToNode[d[0]] = new TreeNode(d[0]);
-        TreeNode* node = (valueToNode[d[1]] == nullptr ? new TreeNode(d[1]) : valueToNode[d[1]]);
-        if (d[2])
-            valueToNode[d[0]]->left = node;
-        else
-            valueToNode[d[0]]->right = node;
-        valueToNode[node->val] = node;
-        isChildNode[d[1]] = true;
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        TreeNode* valueToNode[100001] = {};
+        bool isChildNode[100001] = {};
+        for (auto& d : descriptions){
+            if (parentToChildMap[d[0]] == nullptr) parentToChildMap[d[0]] = new TreeNode(d[0]);
+            TreeNode* node = (parentToChildMap[d[1]] == nullptr ? new TreeNode(d[1]) : parentToChildMap[d[1]]);
+            if (d[2])
+                parentToChildMap[d[0]]->left = node;
+            else
+                parentToChildMap[d[0]]->right = node;
+            parentToChildMap[node->val] = node;
+            isChildNode[d[1]] = true;
+        }
+        for (auto& d : descriptions)
+            if (!isChildNode[d[0]])
+                return parentToChildMap[d[0]];
+        return nullptr;
     }
-    for (auto& d : descriptions)
-        if (!isChildNode[d[0]])
-            return valueToNode[d[0]];
-    return nullptr;
-}
+};
 
 
 int main(){
