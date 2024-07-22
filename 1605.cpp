a6d3761f1cedf77fed4db9e5fc6b14b00d2b4fcc
Patel Muhammed <muhammedpatel538@gmail.com>
2024-07-22 10:01:49 +0530

    1605 Solution

---
+++ 1605.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+ * 1605. Find Valid Matrix Given Row and Column Sums
+*/
+
+class Solution {
+public:
+    vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
+        int numRows = rowSum.size();
+        int numCols = colSum.size();
+        vector<vector<int>> result(numRows, vector<int>(numCols, 0));
+
+        int i = 0, j = 0;
+        while (i < numRows && j < numCols) {
+            int val = min(rowSum[i], colSum[j]);
+            result[i][j] = val;
+            rowSum[i] -= val;
+            colSum[j] -= val;
+
+            if (rowSum[i] == 0) {
+                i++;
+            }
+            if (colSum[j] == 0) {
+                j++;
+            }
+        }
+        return result;
+    }
+};
